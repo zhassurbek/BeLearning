4a0c9a488f977edcae1dd180ce44464831b5f97d
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2023-09-17 13:34:39 +0600

    added:  add @NotNull @NotEmpty update:
    delete:
     - spring security jwt deleted

--- src/main/java/com/zhassurbek/belearning/dao/request/SignUpRequestDto.java
+++ src/main/java/com/zhassurbek/belearning/dao/request/SignUpRequestDto.java
@@ -1,5 +1,7 @@
 package com.zhassurbek.belearning.dao.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +13,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignUpRequestDto {
 
+    @NotNull
+    @NotEmpty
     private String firstName;
+
+    @NotNull
+    @NotEmpty
     private String lastName;
+
+    @NotEmpty
+    @NotNull
     private String email;
+
+    @NotEmpty
+    @NotNull
     private String password;
 
+    @NotNull
+    @NotEmpty
     private String username;
 
 }

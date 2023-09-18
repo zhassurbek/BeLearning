e26fb1d124aff541b75709b4ecb13a133258ea70
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2023-09-18 13:24:01 +0600

    added:  - added @CrossOrigin for turn off CORS update:
    delete:
    
    to do:

--- src/main/java/com/zhassurbek/belearning/controller/AuthenticationController.java
+++ src/main/java/com/zhassurbek/belearning/controller/AuthenticationController.java
@@ -1,23 +1,25 @@
 package com.zhassurbek.belearning.controller;
 
+import com.zhassurbek.belearning.BeLearningApplication;
 import com.zhassurbek.belearning.dao.request.SignInRequestDto;
 import com.zhassurbek.belearning.dao.request.SignUpRequestDto;
 import com.zhassurbek.belearning.dao.response.JwtAuthenticationResponseDto;
 import com.zhassurbek.belearning.service.security.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@CrossOrigin
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+    private final Logger logger = LoggerFactory.getLogger(BeLearningApplication.class);
 
     @PostMapping("/signup")
     public ResponseEntity<JwtAuthenticationResponseDto> signup(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
@@ -26,6 +28,7 @@ public class AuthenticationController {
 
     @PostMapping("/signin")
     public ResponseEntity<JwtAuthenticationResponseDto> signin(@Valid @RequestBody SignInRequestDto signInRequestDto) {
+        logger.info("Received request with username: " + signInRequestDto.getUsername() + " and password: " + signInRequestDto.getPassword());
         return ResponseEntity.ok(authenticationService.signin(signInRequestDto));
     }
 
